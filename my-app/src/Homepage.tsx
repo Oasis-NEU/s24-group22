b9f2aa437efab30b1c65fdd34097b9637bb6da60
ducc <aaronhwang22@gmail.com>
2024-02-17 00:36:45 -0500

    Navigation bar

--- my-app/src/Homepage.tsx
+++ my-app/src/Homepage.tsx
@@ -2,14 +2,32 @@ import './Homepage.css'
 
 function Homepage() {
 
+    
+
     function handleClick() {
         console.log("Button clicked!")
     }
 
+
     return <div className = "homepage_backdrop">
-            <div className = "homepage_dropdown_bar">
-                <button className = "homepage_redirect" onClick={handleClick}> Profile </button>
+            <nav className = "homepage_dropdown_bar">
+                <div className="justified_centering">
+                    <ul>
+                    <a href = "/Projects" className = "homepage_redirect px-4 justify-center" > Projects </a>
+                    <a href = "/People"className = "homepage_redirect px-4"> People </a>
+                    <a href="/Profile" className='homepage_redirect px-4'> Profile </a>
+                    </ul>
+                </div>
+            </nav>
+
+            <div className='homepage_information'> 
+                Welcome to Cinder! 
+                <p>About Cinder: <br></br>
+                    Having trouble finding a project to contribute to? Having decision anxiety? Need a 
+                    kick to get working? Cinder can help you with all of these woes!
+                </p>
             </div>
+            
         </div>
 }
 
