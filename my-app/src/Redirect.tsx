4be7fe0d6776551e0f3f98f6ec9bef80d658c902
eruuc <kim.eric3@northeastern.edu>
2024-02-25 14:01:36 -0500

    Updated Homepage

---
+++ my-app/src/Redirect.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App'
+import Homepage from './Homepage'
+import ProfilePage from './ProfilePage'
+
+export default function Redirect() {
+    return <div>
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Homepage />}></Route>
+            <Route path="/home" element={<Homepage />}></Route>
+            <Route path="/projects" element={<App></App>}></Route>
+            <Route path="/profile" element={<ProfilePage></ProfilePage>}></Route>
+        </Routes>
+    </BrowserRouter>
+    </div>
+}
