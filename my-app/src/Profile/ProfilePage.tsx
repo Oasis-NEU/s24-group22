c1ae7b4aa9da18a4ab791cba902338bab6d2f64c
ducc <aaronhwang22@gmail.com>
2024-03-24 12:27:43 -0400

    header

--- my-app/src/Profile/ProfilePage.tsx
+++ my-app/src/Profile/ProfilePage.tsx
@@ -2,6 +2,14 @@ import "./ProfilePage.css"
 
 export default function ProfilePage() {
     return <div className="ProfilePageBackground">
+
+                    <div className = "flex-box">
+                        <button className = "button test-button" onClick={() => routeChange('/projects')}>Projects</button>
+                        <button className = "button test-button" onClick={() => routeChange('/people')}>People</button>
+                        <button className = "button test-button" onClick={() => routeChange('/hotdog')}>Hotdog</button>
+                        <button className = "button test-button" onClick={() => routeChange('/home')}>Home</button>
+                    </div>
+
             <div className="ProfilePageInformation">
                 <img src="images/mandarin_duck.jpg" className="ProfilePictureBox"></img>
                 <img src="images/mandarin_duck.jpg" className="ProfileBanner"></img>
